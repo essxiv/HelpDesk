20e00188bf29ff2eef6877ffe84f8b3fd757bbb4
eSsXiV <essxiv@gmail.com>
2017-04-17 14:58:00 -0700

    test mocha/chai setup

---
+++ server/tests/db/connection.js
@@ -0,0 +1,6 @@
+const chai = require('chai')
+const mocha = require('mocha')
+
+describe('Test', () => {
+    console.log('Test')
+})
---
+++ server/tests/index.js
@@ -0,0 +1,17 @@
+// Model Tests
+const adminModel_Test = require('./model/adminModel_Test.js')
+const developerModel_Test = require('./model/developerModel_Test.js')
+const staffModel_Test = require('./model/staffModel_Test.js')
+
+// Controller Tests
+const adminController_Test = require('./controller/adminController_Test.js')
+const developerController_Test = require('./controller/developerController_Test.js')
+const staffController_Test = require('./controller/staffController_Test.js')
+
+// Routes Tests
+const adminRoutes_Test = require('./routes/adminRoutes_Test.js')
+const developerRoutes_Test = require('./routes/developerRoutes_Test.js')
+const staffRoutes_Test = require('./routes/staffRoutes_Test.js')
+
+// Database Tests
+const db = require('./db/connection.js')
