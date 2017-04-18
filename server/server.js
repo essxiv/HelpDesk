2a0922b6d22d1772704d608637e6bfbdbb5f9d3d
eSsXiV <essxiv@gmail.com>
2017-04-18 14:50:54 -0700

    server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 const db = require('./db/connection.js')
-
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, function() {
