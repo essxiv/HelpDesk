3de5e338fbdb36864c4d829a950d1e3e064e71aa
eSsXiV <essxiv@gmail.com>
2017-05-07 20:06:30 -0700

    basic react setup

---
+++ client/app/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div id="App">
+                <p>Hello, From App.js</p>
+            </div>
+        )
+    }
+}
+
+export default App
